a665b9ee0320c71e3203c00265872132a0d03fb9
filiz <120565365+filis14@users.noreply.github.com>
2024-04-10 17:09:17 +0300

    Create menuController.java

---
+++ BookManagementApp/src/main/java/app/bookmanagementapp/menuController.java
@@ -0,0 +1,37 @@
+package com.example.menu;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Label;
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class menuController implements Initializable {
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+    @FXML
+    private Label welcomeText;
+
+
+
+
+
+
+    @FXML
+    void openabout(ActionEvent event) {
+
+        Alert alert= new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("About");
+        alert.setContentText("Welcome to the book management application. You can choose the action you want to take. Click 'Search' button to find any book. Click 'Add' button to add a new book. Click 'Tags' button to filter books. Click 'Delete' button to delete a book");
+        alert.setHeaderText("Information");
+        alert.showAndWait();
+
+    }
+
+}
