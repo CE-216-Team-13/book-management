5eeef6f71f809a7d6ad0c61e606bb4bc2f1f4e60
huseyinerenceyhan <120252566+huseyinerenceyhan@users.noreply.github.com>
2024-04-13 12:35:46 +0300

    last update for "books" path
    last version:BookManagementApp\\books

--- BookManagementApp/src/main/java/app/bookmanagementapp/addBookScreen.java
+++ BookManagementApp/src/main/java/app/bookmanagementapp/addBookScreen.java
@@ -158,11 +158,11 @@ public class addBookScreen extends Application {
         book.setTags(new ArrayList<>(List.of(Arrays.toString(TFtags.getText().split(";")))));
         try {
             String filename = generateName();
-            File d = new File("books");
+            File d = new File("BookManagementApp\\books");
             if (!d.exists()) {
                 d.mkdir();
             }
-            Path directory = Paths.get("books", filename);
+            Path directory = Paths.get("BookManagementApp\\books", filename);
             Files.createFile(directory);
             JSONObject json = new JSONObject();
             json.put("title", book.getTitle());
